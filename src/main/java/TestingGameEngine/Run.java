5de2e6007c943a2ba244f798c3320a72a658985f
Karrar <karrraraaljaber@gmail.com>
2021-02-10 13:46:15 +0100

    Commit

--- src/main/java/TestingGameEngine/Run.java
+++ src/main/java/TestingGameEngine/Run.java
@@ -68,7 +68,7 @@ public class Run extends GameCase  {
        // tileMap = new TileMap(500, 500, 32,32, tilesheet);
 
         Sprite s =  new Sprite(playersheet, 24,32);
-        player = new TestPlayer(new Vector2f((WIDTH*SCALE)/2,(HEIGHT*SCALE)/2), 64,64, s);
+        player = new TestPlayer(new Vector2f((WIDTH*SCALE)/3,(HEIGHT*SCALE)/2), 64,64, s);
         cam = new TopDownCamera(player,new Vector2f(0,0), WIDTH ,HEIGHT,1);
 
 
